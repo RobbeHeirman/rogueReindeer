05fccd3b207f87964dd9ec7abee5aad4fabd0cc0
RobbeHeirman <robbeheirman@msn.com>
2021-12-07 15:57:29 +0100

    Runscripts to for smart contract deployment

---
+++ frontend/scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+  const RRNFT = await ethers.getContractFactory("RogueReindeerNFT")
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await RRNFT.deploy()
+  console.log("Contract deployed to address:", myNFT.address)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
+
