031590b52e8dffc24a248c9c8e3922b93aaa96bb
RobbeHeirman <robbeheiman@msn.com>
2021-12-09 16:59:39 +0100

    settings

--- backend/rogue_reindeer/rogue_reindeer/settings.py
+++ backend/rogue_reindeer/rogue_reindeer/settings.py
@@ -54,8 +54,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            # os.path.join(PROJECT_DIR, 'frontend/build'),
-            os.path.join(PROJECT_DIR, 'frontend/src/')
+            os.path.join(PROJECT_DIR, 'frontend/build'),
+            # os.path.join(PROJECT_DIR, 'frontend/src/')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
